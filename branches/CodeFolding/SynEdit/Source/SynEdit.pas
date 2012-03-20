dd48f5f5d22d7cac05b63f812a9ffdffba863a18
kvlahos <kvlahos@d4d5a2a8-bcb1-4873-81f0-44bf4f59e1d2>
2012-03-20 01:14:22 +0000

    CharIndexToRowCol assumes a line break size of two
    git-svn-id: https://svn.code.sourceforge.net/p/synedit/code@77 d4d5a2a8-bcb1-4873-81f0-44bf4f59e1d2

--- branches/CodeFolding/SynEdit/Source/SynEdit.pas
+++ branches/CodeFolding/SynEdit/Source/SynEdit.pas
@@ -9947,8 +9947,9 @@ var
 begin
   RowCol.Line := Min(Lines.Count, RowCol.Line) - 1;
   synEditStringList := (FLines as TSynEditStringList);
+  // CharIndexToRowCol assumes a line break size of two
   Result :=  synEditStringList.LineCharIndex(RowCol.Line)
-           + (RowCol.Char -1);
+           + RowCol.Line * 2 + (RowCol.Char -1);
 end;
 
 procedure TCustomSynEdit.Clear;
